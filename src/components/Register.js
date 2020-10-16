dd28400a15d266badbe89df683dfd3f9e2667eff
Aaron Moran <aaronmorann@protonmail.com>
2020-10-16 17:26:28 +0100

    Username Field

--- src/components/Register.js
+++ src/components/Register.js
@@ -15,9 +15,9 @@ function Register() {
           <div class="register-card">
           <h1>Create an Account</h1>
           <Form>
-          <Form.Group controlId="formBasicEmail">
+          <Form.Group controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="email" placeholder="Create a Username" required/>
+              <Form.Control type="text" placeholder="Create a Username" required/>
               <Form.Text className="text-muted">
               </Form.Text>
             </Form.Group>
